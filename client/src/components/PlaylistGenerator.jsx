23eaebddc7603dbd3ce816d07b78bf3d6698e358
colonelpanic03 <pthkdhir@gmail.com>
2025-08-19 16:03:05 -0400

    move playlist generator logic to dedicated component

---
+++ client/src/components/PlaylistGenerator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PlaylistGenerator({ likedSongs, accessToken }) {
+  // Move the component logic here since it was misplaced in pages folder
+  // This is the actual component that should be used
+  return (
+    <div>Playlist Generator Component - Implementation moved to separate file</div>
+  );
+}
+
+export default PlaylistGenerator;
